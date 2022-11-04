4dab8201acdfee588d3f1488e935c491f7ebba0a
Riba00 <jordiriba@iesebre.com>
2022-11-04 11:48:59 +0100

    Tot acabat i funciona

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,5 +1,52 @@
 <x-layout>
     <div>
-        users to do
+        <div class="min-h-full">
+            <header class="bg-white shadow">
+                <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Users</h1>
+                </div>
+            </header>
+            <main>
+                <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+
+                    <div class="overflow-x-auto relative">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="py-3 px-6">
+                                    Id
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Name
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Email
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Password
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td class="py-4 px-6">{{ $user->id }}</td>
+                                    <td class="py-4 px-6">
+                                        {{ $user->name }}
+                                    </td>
+                                    <td class="py-4 px-6">
+                                        {{ $user->email }}
+                                    </td>
+                                    <td class="py-4 px-6">
+                                        {{ $user->pasword }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\TascaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,17 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [PagesController::class,'contact']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about',[PagesController::class,'about']);
 
-Route::get('/users', function () {
-    return view('users');
-});
+Route::get('/users', [UserController::class,'index']);
 
 Route::get( '/', [TascaController::class,'index']);
 
